9159b3ae87cd6faf34572ac32788f0106238ffa6
Florian Heinemann <florian.heinemann@gmail.com>
2014-06-14 14:10:06 +0200

    Hashing & Salting

--- test/mongostore.js
+++ test/mongostore.js
@@ -121,4 +121,51 @@ describe('Specific tests', function() {
 			});
 		})
 	})
+
+	it('should store tokens only in their hashed form', function (done) {
+		var store = TokenStoreFactory();
+		var token = uuid.v4();
+		var uid = chance.email();
+		store.storeOrUpdate(token, uid, 
+			1000*60, 'http://' + chance.domain() + '/page.html', 
+			function() {
+				MongoClient.connect(testUri, function(err, db) {
+					db.collection('passwordless-token', function(err, collection) {
+						collection.findOne({uid: uid}, function(err, item) {
+							expect(item.uid).to.equal(uid);
+							expect(item.hashedToken).to.not.equal(token);
+							done();
+						});
+					});
+				})
+			});
+	})
+
+	it('should store tokens not only hashed but also salted', function (done) {
+		var store = TokenStoreFactory();
+		var token = uuid.v4();
+		var uid = chance.email();
+		store.storeOrUpdate(token, uid, 
+			1000*60, 'http://' + chance.domain() + '/page.html', 
+			function() {
+				MongoClient.connect(testUri, function(err, db) {
+					db.collection('passwordless-token', function(err, collection) {
+						collection.findOne({uid: uid}, function(err, item) {
+							var hashedToken1 = item.hashedToken;
+							store.clear(function() {
+								store.storeOrUpdate(token, uid, 
+									1000*60, 'http://' + chance.domain() + '/page.html', 
+									function() {
+										collection.findOne({uid: uid}, function(err, item) {
+											var hashedToken2 = item.hashedToken;
+											expect(hashedToken2).to.not.equal(hashedToken1);
+											done();
+										});
+									});
+							})
+						});
+					});
+				})
+			});
+	})
 })
