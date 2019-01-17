386b8071789f85f753dce7e1c3523ab0f5fa2670
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2019-01-17 16:17:15 +0100

    UPDATE node-uuid to uuid

--- test/mongostore.js
+++ test/mongostore.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var expect = require('chai').expect;
-var uuid = require('node-uuid');
+var uuid = require('uuid');
 var chance = new require('chance')();
 
 var MongoStore = require('../');
